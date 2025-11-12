d76382a5504ad268f19bb7385eeff8ebb804a387
guilherme-donato-dev <donatocruzg@gmail.com>
2025-11-12 14:46:05 -0300

    preparando par ao deploy

--- app/settings.py
+++ app/settings.py
@@ -31,9 +31,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%0&#g_b^aq=ufv-sih2ww+xnmofh#1@^+-qvyjf=#(w+l*+ni#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.onrender.com']
 
 
 # Application definition
